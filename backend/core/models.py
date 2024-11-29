02ae20ca83d493159c5f6113a9e33c2edd8af879
Hesch <heschmat.seyedi@gmail.com>
2024-11-29 23:18:35 +0100

    Modify *Recipe* model to handle image.

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,6 +1,9 @@
 """
 Database models.
 """
+import os
+import uuid
+
 from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import (
@@ -8,6 +11,41 @@ from django.contrib.auth.models import (
     BaseUserManager,
     PermissionsMixin
 )
+
+
+def get_path_for_recipe_img(instance, filename):
+    """
+    Generate a dynamic file path for an uploaded recipe image.
+
+    This function is called by the `upload_to` argument of the `ImageField`
+    in the `Recipe` model to determine the storage location of the uploaded
+    file. The path includes a unique filename generated using a UUID to ensure
+    there are no naming conflicts.
+
+    Args:
+        instance: The model instance to which the image is being uploaded.
+                  Although not explicitly used in this implementation, it provides
+                  context about the model (e.g., access to fields like `instance.id`
+                  or `instance.user`) and can be leveraged for custom file paths.
+        filename: The original name of the uploaded file, including its extension.
+
+    Returns:
+        str: A string representing the file path where the image will be stored.
+             The path format is 'uploads/recipe/<unique_uuid>.<file_extension>'.
+
+    Example:
+        If a user uploads a file named 'example.jpg', the generated path might be:
+        'uploads/recipe/123e4567-e89b-12d3-a456-426614174000.jpg'.
+
+    Notes:
+        - The UUID ensures that each uploaded file has a unique name, avoiding
+          collisions even if multiple users upload files with the same name.
+        - The `instance` argument allows for customization of the path based on
+          model attributes if needed, though it is not used in the current implementation.
+    """
+    ext = os.path.splitext(filename)[1]
+    filename = f'{uuid.uuid4()}{ext}'
+    return os.path.join('uploads', 'recipe', filename)
 
 
 # Create your models here. ======================================================== #
@@ -54,6 +92,7 @@ class Recipe(models.Model):
     link = models.URLField(max_length=250, blank=True)
     tags = models.ManyToManyField('Tag')
     ingredients = models.ManyToManyField('Ingredient')
+    image = models.ImageField(null=True, upload_to=get_path_for_recipe_img)
 
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,  # set to 'core.User' in config/setttings.py
